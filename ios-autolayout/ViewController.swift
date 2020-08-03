3c78564c2bead215240c916a70ca41495b47082f
LM Bibera <lyndon.bibera@headhuntr.io>
2020-08-03 14:46:38 +0800

    Initial Commit

---
+++ ios-autolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-autolayout
+//
+//  Created by StartupBuilder.INFO on 8/3/20.
+//  Copyright © 2020 StartupBuilder.INFO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
